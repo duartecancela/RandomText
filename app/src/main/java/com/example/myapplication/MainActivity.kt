4f6b4faf7b4ba841df6f0ff5377ecfc69eb369a0
Duarte Cancela <duartecancela@gmail.com>
2021-10-13 20:46:35 +0100

    Add counter with buttons increase / decrease

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,9 +7,14 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val texViewCounter = findViewById<TextView>(R.id.textViewCounter)
+        texViewCounter.text = count.toString()
     }
 
     fun newGreeting(view: android.view.View) {
@@ -18,5 +23,18 @@ class MainActivity : AppCompatActivity() {
         val greeting = greetings[Random.nextInt(0, greetings.size -1)]
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = greeting
+    }
+
+    fun increaseCounter(view: android.view.View) {
+        count++
+        val texViewCounter = findViewById<TextView>(R.id.textViewCounter)
+        texViewCounter.text = count.toString()
+        Toast.makeText(applicationContext,"Increase",Toast.LENGTH_SHORT).show()
+    }
+    fun decreaseCounter(view: android.view.View) {
+        count--
+        val texViewCounter = findViewById<TextView>(R.id.textViewCounter)
+        texViewCounter.text = count.toString()
+        Toast.makeText(applicationContext,"Decrease",Toast.LENGTH_SHORT).show()
     }
 }
